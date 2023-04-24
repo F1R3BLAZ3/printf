5e490fa60eaa9501e49ab42bb1f3bc259e42e3e4
Ckola99 <Christopherkola@gmail.com>
2023-04-24 22:15:11 +0200

    get function for printing format specifiers file

---
+++ get_print_func.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * get_print_func - Gets the appropriate print function for a given format specifier.
+ * @type: The format specifier character.
+ *
+ * Return: A pointer to the appropriate print function, or NULL if the specifier is invalid.
+ */
+
+int (*get_print_func(char type))(va_list)
+{
+	print_func_t print_funcs[] = {
+	    {'c', print_char},
+	    {'s', print_string},
+	    {0, NULL}};
+
+	int i = 0;
+
+	while (print_funcs[i].type)
+	{
+		if (print_funcs[i].type == type)
+			return (print_funcs[i].func);
+
+		i++;
+	}
+
+	return (NULL);
+}
