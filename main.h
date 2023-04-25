0fd6e28e13fe5af8e31c9a38d1aafa8c459e1c56
F1R3BLAZ3 <zandre.777.deboer@gmail.com>
2023-04-25 11:42:24 +0200

    Revert "Updated main.h"
    This reverts commit 3b8bc86ac43b9d5aa4e37c48c1e6c00aecc88685.

--- main.h
+++ main.h
@@ -3,6 +3,17 @@
 
 #include <stdarg.h>
 
+/**
+ * struct print_func - Struct containing a char and a pointer to a function.
+ * @type: The character format specifier.
+ * @func: Pointer to a function that handles the corresponding format specifier
+ */
+typedef struct print_func
+{
+	char *type;
+	int (*func)(va_list);
+} print_func_t;
+
 int _printf(const char *format, ...);
 int print_char(va_list *args);
 int print_string(va_list *args);
