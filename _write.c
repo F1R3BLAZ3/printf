03c9d08d8beee4a8ccab8f150758390c8536089c
Ckola99 <Christopherkola@gmail.com>
2023-04-25 15:50:50 +0200

    betty changes

--- _write.c
+++ _write.c
@@ -1,11 +1,12 @@
 #include <unistd.h>
 
 /**
-  *write_char - Writes a char.
-  *@s: Character to be written.
-  *Return: 1 on success and -1 on error.
-  */
+ *write_char - Writes a char.
+ *@s: Character to be written.
+ *Return: 1 on success and -1 on error.
+ */
+
 int _write(char s)
 {
-	return (write(1, &s, 1));
+  return (write(1, &s, 1));
 }
