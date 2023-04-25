d1d7ea9efb0dc0aaa3c1bdbe7bcca5568de71d8d
F1R3BLAZ3 <zandre.777.deboer@gmail.com>
2023-04-25 11:33:34 +0200

    Updated main.h

--- main.h
+++ main.h
@@ -4,10 +4,10 @@
 #include <stdarg.h>
 
 int _printf(const char *format, ...);
-int print_char(va_list args);
-int print_string(va_list args);
-int print_percent(va_list args);
-int print_int(va_list args);
+int print_char(va_list *args);
+int print_string(va_list *args);
+int print_percent(va_list *args);
+int print_int(va_list *args);
 int print_decimal(va_list args);
 int print_unsigned_decimal(va_list args);
 int print_octal(va_list args);
