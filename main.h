cf87b28596015e0428b60cd2b134b6ef3fa626ca
Ckola99 <Christopherkola@gmail.com>
2023-04-25 23:50:10 +0200

    modified scripts

--- main.h
+++ main.h
@@ -22,7 +22,7 @@ int print_char(va_list args);
 int print_string(va_list args);
 int print_percent(va_list args);
 int print_int(va_list args);
-int print_decimal(va_list args);
+int print_dec(va_list args);
 int print_unsigned_decimal(va_list args);
 int print_octal(va_list args);
 int print_hexidecimal(va_list args);
