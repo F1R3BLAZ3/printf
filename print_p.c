cc41695924b08340447acf5aa5cbc00803e13d60
F1R3BLAZ3 <zandre.777.deboer@gmail.com>
2023-04-28 22:54:58 +0200

    Revert "removed print_p"
    This reverts commit e1b605302e6e79713c93cfbc86c69c87af99b811.

---
+++ print_p.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+/**
+ * print_pointer - Prints memory address of a pointer to stdout.
+ * @args: A list of arguments containing the pointer
+ *        whose memory address is to be to printed.
+ *
+ * Return: The number of characters printed.
+ */
+
+int print_pointer(va_list args)
+{
+	void *p;
+
+        p = va_arg(args, void*);
+        
+}
